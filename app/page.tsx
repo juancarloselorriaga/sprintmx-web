0502b9a3984413270d9853597f8ebd2a2cb8c016
Juan Carlos Elorriaga <juancarloselorriaga@gmail.com>
2025-11-17 21:46:40 +0100

    fix(app): adjust div width to occupy full screen width

--- app/page.tsx
+++ app/page.tsx
@@ -33,7 +33,7 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center">
+    <div className="w-full flex min-h-screen items-center justify-center">
       <p>
         Home
       </p>
