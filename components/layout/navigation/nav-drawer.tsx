2fafcd0413eedf0f23013d147c3e84b5c2a0230c
Juan Carlos Elorriaga <juancarloselorriaga@gmail.com>
2025-12-08 19:22:32 +0100

    fix: animation

--- components/layout/navigation/nav-drawer.tsx
+++ components/layout/navigation/nav-drawer.tsx
@@ -40,7 +40,7 @@ export function NavigationDrawerContent({
       className="p-0"
     >
       <div
-        className="flex h-full flex-col origin-left px-0 py-0 opacity-0 scale-[0.97] translate-x-[-4px] transform-gpu transition-[opacity,transform] duration-[220ms] ease-[cubic-bezier(0.18,0.89,0.32,1.28)] delay-75 group-data-[state=closed]/sheet:delay-0 group-data-[state=open]/sheet:opacity-100 group-data-[state=open]/sheet:scale-100 group-data-[state=open]/sheet:translate-x-0"
+        className="flex h-full flex-col origin-top px-0 py-0 opacity-0 scale-[0.97] translate-x-[-4px] translate-y-2 transform-gpu transition-[opacity,transform] duration-[230ms] ease-[cubic-bezier(0.2,0.7,0.25,1)] delay-50 group-data-[state=closed]/sheet:delay-0 group-data-[state=open]/sheet:opacity-100 group-data-[state=open]/sheet:scale-100 group-data-[state=open]/sheet:translate-x-0 group-data-[state=open]/sheet:translate-y-0"
         data-open={open ? 'true' : 'false'}
       >
         <SheetHeader className="p-1 py-2 border-b">
